5cad66e0b8d0c9d2ff62dab4e6a811e70c068e73
lanaeleven <maulanaelvn@gmail.com>
2026-08-12 09:47:49 +0800

    feat: :sparkles: buka semua informasi untuk akun direktur

--- app/Http/Controllers/InformasiController.php
+++ app/Http/Controllers/InformasiController.php
@@ -55,9 +55,17 @@ class InformasiController extends Controller
     {
         $userId = auth()->user()->id;
 
-        $informasi = Informasi::whereHas('users', function ($query) use ($userId) {
+        $userId = auth()->user()->id;
+        if ($userId == 3) {
+            $informasi = Informasi::orderBy('tahun', 'desc')->orderBy('index', 'desc');
+        } else {
+            $informasi = Informasi::whereHas('users', function ($query) use ($userId) {
             $query->where('users.id', $userId);
         });
+        }
+        // $informasi = Informasi::whereHas('users', function ($query) use ($userId) {
+        //     $query->where('users.id', $userId);
+        // });
         $jenisInformasi = JenisInformasi::all();
 
         $judul = "Informasi";
