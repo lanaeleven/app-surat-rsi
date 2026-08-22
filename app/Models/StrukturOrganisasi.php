c1b69cdfeead0bb176d7101d25fdb5e04990c418
lanaeleven <maulanaelvn@gmail.com>
2026-08-22 12:50:44 +0800

    feat: :sparkles: tambah casting levelJabatan pada model StrukturOrganisasi

--- app/Models/StrukturOrganisasi.php
+++ app/Models/StrukturOrganisasi.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\LevelJabatan;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -12,6 +13,10 @@ class StrukturOrganisasi extends Model
 
     protected $guarded = [
         'id'
+    ];
+
+    protected $casts = [
+        'levelJabatan' => LevelJabatan::class,
     ];
 
     protected $table = 'struktur_organisasi';
